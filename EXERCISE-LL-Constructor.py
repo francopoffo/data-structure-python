23090679ceaac81455bc1d130235457bf8ab37ab
Franco Poffo <franco_sutter@yahoo.com.br>
2023-03-31 10:57:11 -0300

    Exercício 2 - LL print, append

--- EXERCISE-LL-Constructor.py
+++ EXERCISE-LL-Constructor.py
@@ -1,14 +1,20 @@
 class Node:
-  def __init__(self, value):
-    self.value = value
-    self.next = None
+    def __init__(self, value):
+        self.value = value
+        self.next = None
         
 class LinkedList:
-  def __init__(self, value):
-    new_node = Node(value)
-    self.head = new_node
-    self.tail = new_node   
-    self.length = 1
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node   
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
  
 my_linked_list = LinkedList(4)
@@ -16,15 +22,7 @@ my_linked_list = LinkedList(4)
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length)
+print(my_linked_list.print_list())
 
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    Head: 4
-    Tail: 4
-    Length: 1
-    
-"""
 
                                                                                                                     
